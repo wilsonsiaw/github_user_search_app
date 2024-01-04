65e79c43e38f4982fbcd700c29376c584a14809d
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-04 16:42:09 +0200

    ✨ feat(Header component): Add content
    Add the header content to be rendered. The content also includes the text and a logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import moon from '../../assets/moon.svg'
+
+const Header = () => {
+  return (
+    <div className='headerWrapper'>
+      <h1>devfinder</h1>
+      <div className="headerRight">
+          <h4>DARK</h4>
+          <img src={moon} alt="A half moon image" />
+      </div>
+    </div>
+  )
+}
+
+export default Header

8e97c7bae1703fda4d10f46565b0f8fbe073d701
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-06 20:11:38 +0200

    ✨ feat(Search component): Import theme
    Import and use the dark mode theme in the component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './Search.css'
 import search from '../../assets/search.svg'
+import { useTheme } from '../../context/ThemeContextProvider'
 
 const Search = () => {
+
+  const { isDarkMode } = useTheme()
+  
   return (
     <div className="formWrapper">
-        <form className='form'>
+        <form className={`form ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
             <img src={search} alt="A search image" className='searchImg'/>
-            <input type="text" id='search' placeholder='Search GitHub username...'/>
+            <input type="text" id='search' 
+            placeholder='Search GitHub username...' className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`}/>
             <button className='searchBtn'>Search</button>
         </form>
     </div>

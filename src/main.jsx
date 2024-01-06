450c668e053a9020dcb055bf87024bd8ac111f84
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-06 20:05:02 +0200

    🦄 refactor(Main): Wrap Application
    Wrap the root application component with the theme context provider to make it available to all components in the application

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeContextProvider } from './context/ThemeContextProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>,
 )

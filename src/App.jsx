745fb74fafe279a3313167716e6ff83c87a9e261
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-06 20:03:34 +0200

    ✨ feat(Application component): Use theme
    Import and use the theme. Also imort and use the toggle theme function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './App.css'
 import Search from './components/Search/Search'
 import Header from './components/Header/Header'
 import Card from './components/Card/Card'
+import { ThemeContext, useTheme } from './context/ThemeContextProvider'
 
 function App() {
 
+  // import and use the theme from the 
+  // Theme Context Provider file
+  const { isDarkMode, toggleTheme } = useTheme()
+
   return (
-    <div className='appBody'>
-      <Header />
+    <div className={`appBody ${isDarkMode ? 'dark-mode' : 'light-mode'}`} >
+      <Header toggleTheme={toggleTheme}/>
       <Search />
       <Card />
     </div>

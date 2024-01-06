54a8d683b1d52d31f635f383f9c86d74557b4def
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-06 20:08:57 +0200

    ✨ feat(Header component): Use theme
    Import and use the dark mode theme in the component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Header.css'
 import moon from '../../assets/moon.svg'
+import sun from '../../assets/sun.svg'
+import { useTheme } from '../../context/ThemeContextProvider'
 
-const Header = () => {
+const Header = ({toggleTheme}) => {
+
+  const {isDarkMode} = useTheme()
+
   return (
-    <div className='headerWrapper'>
+    <div className={`headerWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <h1>devfinder</h1>
-      <div className="headerRight">
-          <h4>DARK</h4>
-          <img src={moon} alt="A half moon image" />
+      <div className="headerRight" onClick={toggleTheme}>
+          <h4>{isDarkMode ? "LIGHT" : "DARK"}</h4>
+          { isDarkMode ? <img src={sun} alt="A half moon image" /> : 
+          <img src={moon} alt="A half moon image" />} 
       </div>
     </div>
   )

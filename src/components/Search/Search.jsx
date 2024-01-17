16ea65668cfa343048c910cb0599a338ac174ddb
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-17 11:18:56 +0200

    ✨ feat(Search component): Build form
    Add the form functionality and also refactor the component to make use of the dark mode theme

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
 import search from '../../assets/search.svg'
 import { useTheme } from '../../context/ThemeContextProvider'
 
-const Search = () => {
+const Search = ({getGithubUser}) => {
 
+  // get the theme
   const { isDarkMode } = useTheme()
+
+  // create a controlled form input
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = () => {
+    if (!query) {
+      alert('Field is required');
+    } else {
+      getGithubUser(query);
+      setQuery('');
+    }
+  }
   
   return (
     <div className="formWrapper">
         <div className={`form ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
             <img src={search} alt="A search image" className='searchImg'/>
-            <input type="text" id='search' 
+            <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" id='search' 
             placeholder='Search GitHub username...' className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`}/>
-            <button className='searchBtn'>Search</button>
+            <button className='searchBtn' onClick={handleSubmit}>Search</button>
         </div>
     </div>
   )

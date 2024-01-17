54224dac107b335780782c0198de5d45c512b5fb
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-17 11:15:12 +0200

    🦄 refactor(Card component): Dark mode style
    Refactor the code to make use of the dark mode theme

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,41 +6,43 @@ import link from '../../assets/link.svg'
 import twitter from '../../assets/twitter.svg'
 import github from '../../assets/github.svg'
 import { useTheme } from '../../context/ThemeContextProvider'
+import {format, parseISO} from 'date-fns'
 
-const Card = () => {
+const Card = ( {githubUser} ) => {
 
   const { isDarkMode } = useTheme()
 
   return (
     <div className={`cardWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <div className={`cardTop ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-        <img src={oval} alt="A picture of a black cat in an oval" />
+        {!githubUser?.avatar_url ? (<img src={oval} alt="A picture of a black cat in an oval" />) 
+        : (<img src={githubUser?.avatar_url} alt="A picture ot the github user" />)}
         <div className="topText">
-          <h3>The Octocat</h3>
-          <h4>@octocat</h4>
-          <h4>Joined 25 Jan 2011</h4>
+          <h3>{githubUser?.name === null ? "The Octocat" : githubUser?.name}</h3>
+          <h4>{githubUser?.login === null ? "The Octocat" : githubUser?.login}</h4>
+          <h4>{githubUser && `Joined ${format(parseISO(githubUser?.created_at), 'd MMM yyyy')}`}</h4>
         </div>
       </div>
       <p className={`para ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.
+      {githubUser?.bio === null ? "No GitHub Bio" : githubUser?.bio}
       </p>
       <div className={`stats ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
         <small>Repos</small>
         <small>Followers</small>
         <small>Following</small>
-        <h5>8</h5>
-        <h5>3938</h5>
-        <h5>9</h5>
+        <h5>{githubUser?.public_repos === null ? "No GitHub Repos" : githubUser?.public_repos}</h5>
+        <h5>{githubUser?.followers === null ? "No GitHub Followers" : githubUser?.followers}</h5>
+        <h5>{githubUser?.following === null ? "Not Following" : githubUser?.following}</h5>
       </div>
       <div className={`icons ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
         <img src={pin} alt="location pin icon" id='icon'/>
-        <p>San Francisco</p>
+        <p>{githubUser?.location === null ? "Not Following" : githubUser?.location}</p>
         <img src={link} alt="a link icon" />
-        <p>https://github.blog</p>
+        <p>{githubUser?.public_gists === " " ? "No GitHub Blog" : githubUser?.public_gists}</p>
         <img src={twitter} alt="a twitter bird icon" />
-        <p>Not available</p>
+        <p>{githubUser?.twitter_username === null ? "No Twitter Username" : githubUser?.twitter_username}</p>
         <img src={github} alt="an office building icon" />
-        <p>@github</p>
+        <p>{githubUser?.company === null ? "No Company" : githubUser?.company}</p>
       </div>
     </div>
   )
